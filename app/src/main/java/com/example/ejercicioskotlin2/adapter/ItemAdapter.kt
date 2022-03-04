a74dd4817984acba6d16041e003dc69012998deb
Unai <unaimartinezurraca@hotmail.com>
2022-03-04 22:08:19 +0100

    Ejercicio9

--- app/src/main/java/com/example/ejercicioskotlin2/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/ejercicioskotlin2/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ejercicioskotlin2.R
@@ -12,11 +13,11 @@ import com.example.ejercicioskotlin2.model.Affirmation
 class ItemAdapter(
     private val context: Context,
     private val dataset: List<Affirmation>
-) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
 
@@ -32,6 +33,7 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
 
